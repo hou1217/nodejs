a1ae30e4c6141377dec858eeb8cae7977209a1ea
houyishuai <hou1217@foxmail.com>
2019-12-28 11:54:50 +0800

    2019年12月28日11:54:40

--- src/http/server.js
+++ src/http/server.js
@@ -1,9 +1,19 @@
-let http = require('http')
-let server = http.createServer()
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer()
 server.on('request',(req,res)=>{
   console.log(req.method);
-  res.write('hello\n');
-  res.end();
+  console.log(req.url);
+  if(req.url == '/'){
+    res.write('hello\n');
+    res.end();
+  }else{
+    fs.readFile('.'+req.url,(err,data)=>{
+      if(err) throw err;
+      res.end(data);
+    })
+  }
 })
 server.listen(8888,()=>{
   // 终端打印如下信息
